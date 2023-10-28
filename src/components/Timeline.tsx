7b7d9d8ae4d328d90f80fac744c5a778dd1cb801
vineas <alvienasyandika@gmail.com>
2023-10-28 22:14:22 +0700

    edit intro vineas

--- src/components/Timeline.tsx
+++ src/components/Timeline.tsx
@@ -1,6 +1,6 @@
 const Timeline = () => {
   return (
-    <div className='border-2 shadow-xl rounded-xl overflow-hidden justify-center pr-10 pl-10' style={{ width: '80%', margin: 'auto' }}>
+    <div className='border-2 shadow-xl rounded-xl overflow-hidden justify-center pr-10 pl-10' style={{ width: '100%', margin: 'auto' }}>
       <div className='flex items-center justify-center text-justify pt-6 p'>
         <p className="text-md w-lg mb-6 ">
           I am a Software Engineer with a primary interest in modern web application development. I have extensive experience in building and designing web applications that are responsive, intuitive, and reliable. I also have a background in programming languages such as JavaScript, PHP, and Python.
